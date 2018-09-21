091d99216ca92ea783b2fe6aacbc42bc59208589
PinkalGandhi <42044440+PinkalGandhi@users.noreply.github.com>
2018-09-21 17:22:39 +0530

    Update GDriveUpload.php

--- FaceBookAlbum/GoogleDrive/GDriveUpload.php
+++ FaceBookAlbum/GoogleDrive/GDriveUpload.php
@@ -1,121 +1,146 @@
 <?php
-  
-    require_once 'GoogleConfig.php';
 
+    spl_autoload_register();
+    require_once 'Config.php';
+    //increase maximum execution time
+    ini_set('max_execution_time', 900);
     if(isset($_GET['state'])) {
 
         $_SESSION['FBRLH_state'] = $_GET['state'];
     }
 
     $accessToken = $_SESSION['facebookAccessToken'];
+    //echo $accessToken;
 
-    if(!isset($accessToken))
+    if(isset($_GET['id']))
     {
-        header('Location : index.php');
+        $_SESSION['album_id'] = $_GET['id'];
     }
+    $userAlbumId = $_SESSION['album_id'];
+    //echo $userAlbumId;
+
+    require_once 'GoogleConfig/vendor/autoload.php';
+
+    $url_array = explode('?', 'http://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+
+    $client = new Google_Client(); //create class for google clent
     
-    $album_id = $_GET['id'];
-    //echo $album_id;
-    //picture,images,name,created_time&limit=100
-    $photos = $FB->get("/{$album_id}/photos?fields=picture,images,name&limit=600", $accessToken)->getGraphEdge()->asArray();
-    $images=array();
-
-    foreach($photos as $photo)
-    {
-        array_push($images,$photo['images'][0]['source']);
-    }
-    //echo json_encode($images);
-
-    $service = new Google_Service_Drive($client);
-	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$client->setClientId('818891243832-g8prnv3qfskd4fvgndf4512t06fjru45.apps.googleusercontent.com');//client id
+	$client->setClientSecret('tlFxfsf1nAR8ysx8Gk_3h5F2');//clent secret
+	$client->setRedirectUri($url_array[0]);
+	$client->addScope(Google_Service_Drive::DRIVE);
+	$client->setScopes(array('https://www.googleapis.com/auth/drive'));
+	if(isset($_GET['code']))
+	{
+		$client->authenticate($_GET['code']);
+		$access_token = $client->getAccessToken();
+		$client->setAccessToken($access_token);
+	}
+	else
+	{
+		$auth_url = $client->createAuthUrl();
+		header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+	}
 		
-    //if check directory name exist or not on google drive if not exist the create directory
-    $file = new Google_Service_Drive_DriveFile();
-    if(in_array("FaceBook Album Downloder",$foldername))
-    {
-        $folderId =$foldername_and_id['FaceBook Album Downloder'] ;
+	$service = new Google_Service_Drive($client);
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+
+    // $dir = 'FACEBOOK_ALBUM_DOWNLOADER';
+    // $fileMetadata = new Google_Service_Drive_DriveFile(array(
+    //     'name' => $dir,
+    //     'mimeType' => 'application/vnd.google-apps.folder'));
+    //     $file = $service->files->create($fileMetadata, array(
+    //         'fields' => 'id'));
+        
+    //printf("Folder ID: %s\n", $file->id);
     
-    }
-    else
+    $url = "https://graph.facebook.com/v3.1/me?fields=id,name,albums{id,count,name,cover_photo,photo_count,photos{picture,likes{id}},likes,link},likes&access_token=".$accessToken;
+    $result = file_get_contents($url);
+    $album = json_decode($result);
+    
+    foreach($album->albums->data as $userdata)
     {
-        $fileMetadata = new Google_Service_Drive_DriveFile(array(
-			'name' => 'FaceBookAlbums',
-			'mimeType' => 'application/vnd.google-apps.folder'));
-			
-			$file = $service->files->create($fileMetadata, array(
-			'fields' => 'id'));
-			$folderId = $file->id;
-		
-    }
-
-    if(isset($album_id))
-    {
-        foreach($album_id as $albumID)
+        if($userdata->id == $userAlbumId)
         {
-            $id = $albumID;
-            $getAlbumname = $fb->get('/'.$id.'?fields=name,photos.limit(100){images,name,created_time}',$_SESSION['facebookAccessToken'])->getGraphEdge()->asArray();
-            $folder_name = $getAlbumname['name'];
+            $albumId = $userAlbumId;
+            $response = $FB->get('/'.$userAlbumId.'/?fields=name',$_SESSION['facebookAccessToken']);
+            $album = $response->getGraphNode()->asArray();
+            $albumname = $album['name'];
+            //echo $albumname;
 
-            //creating google directory on google drive
-
+            //google drive folder
             $fileMetadata = new Google_Service_Drive_DriveFile(array(
-				'name' => $folder_name,
-				'parents' => array($folderId),
-				'mimeType' => 'application/vnd.google-apps.folder'));
-				
-				$file = $service->files->create($fileMetadata, array(
-				'fields' => 'id'));
-                $folderid = $file->id;
-                
-            //fetching images from the album
+                'name' => $albumname,
+                'mimeType' => 'application/vnd.google-apps.folder'));
+                $file = $service->files->create($fileMetadata, array(
+                'fields' => 'id'));
+                 $folder_id = $file->id;
 
-            $url1="https://graph.facebook.com/v3.1/".$id."/photos?fields=images%2Calbum&access_token=".$fbAccessToken;
-            $result = file_get_contents($url1);
-            $pic=json_decode($result);
-            $existphotokey=(array)$pic;
+            //fetch images album wise
+            $url = "https://graph.facebook.com/v3.1/".$albumId."/photos?fields=name,images%2Calbum&access_token=".$accessToken;
+            $result = file_get_contents($url);
+            $pic = json_decode($result);
+            $existphotokey = (array)$pic;
             $a=(array)$pic->paging;
-
             if(array_key_exists("next",$a))
             {
-                $url1=$a["next"];
+                $url=$a["next"];
             }
-            do
-            {
-                if(array_key_exists("next",$a))
-                {
-                    $url1=$a["next"];
-                }
-                else{
-                    $url1 = "none";
-                }
+            //paging
+                do{
+                    if(array_key_exists("next",$a))
+                    {
+                        $url=$a["next"];
+                    }
+                    else{
+                        $url="none";
+                    }
+                    foreach($pic->data as $mydata)
+                    {
+                        $url1=$mydata->images[1]->source;
+                        // $photoId=$mydata->id;
+                        // $file=$path.$photoId.'.jpg';
+                        // file_put_contents($file,file_get_contents($url1));
+                        $imgurl = rand(1,999999).rand(1,999999).'.jpg';
 
-                foreach($pic->data as $mydata)
-                {
-                    $url = $mydata->images[0]->source;
-                    $img = rand(1,9999999).rand(1,99999999).'.jpg';
-                    $fileMetadata = new Google_Service_Drive_DriveFile(array(
-                        'name' => $img,
-                        'parents' => array($folderid)
-                        ));
-                        $content = file_get_contents($url);
-                        $file = $service->files->create($fileMetadata, array(
-                        'data' => $content,
-                        'mimeType' => 'image/jpeg',
-                        'uploadType' => 'multipart',
-                        'fields' => 'id'));	
-                }
-                if($url1!="none")
-                {
-                    $result = file_get_contents($url1);
-                }
-                $pic = json_decode($result);
-                $existphotokey = (array)$pic;
-                $a = (array)$pic->paging;
-            }while($url1!="none");
+                            //moving albums to the drive
+
+                            $fileMetadata = new Google_Service_Drive_DriveFile(array(
+                                'name' => $imgurl,
+                                'parents' => array($folder_id)
+                                ));
+                                $content = file_get_contents($url1);
+                                $file = $service->files->create($fileMetadata, array(
+                                'data' => $content,
+                                'mimeType' => 'image/jpeg',
+                                'uploadType' => 'multipart',
+                                'fields' => 'id'));
+                    }
+                    if($url!="none")
+                    {
+                        $result=file_get_contents($url);
+                    }
+                    $pic=json_decode($result);
+                    $existphotokey=(array)$pic;
+                    $a=(array)$pic->paging;
+                }while($url!="none");
         }
     }
     finfo_close($finfo);
     header('Location : Album.php');
     exit();
+    
+    // $album_id = $_GET['id'];
+    // echo $album_id;
+    // //picture,images,name,created_time&limit=100
+    // $photos = $FB->get("/{$album_id}/photos?fields=picture,images,name&limit=600", $accessToken)->getGraphEdge()->asArray();
+    // $images=array();
+
+    // foreach($photos as $photo)
+    // {
+    //     $images[] = $photo['images'][0]['source'];
+    // }
+
+    // echo json_encode($images);
 
 ?>
